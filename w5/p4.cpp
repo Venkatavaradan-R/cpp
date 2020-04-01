522648a0c5f794a0921b050282d1e6c3f532ad49
Venkatavaradan R <venkatavaradan.r@gmail.com>
2020-04-01 19:41:32 +0530

    added half a prog

---
+++ w5/p4.cpp
@@ -0,0 +1,19 @@
+// fn to strcmp
+
+#include<iostream>
+
+int strcomp(char*, char*);
+
+int main()
+{
+    std::cout<<"the out put of comparing bad and Bad is: "<< strcomp("bad", "Bad");
+}
+
+int strcomp(char * st1, char* st2)
+{
+    int i=0;
+    while (1)
+    {
+        if
+    }
+}
