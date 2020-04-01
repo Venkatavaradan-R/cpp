24aff3315474af9ece9a06d52d7ffcb455a00030
Venkatavaradan R <venkatavaradan.r@gmail.com>
2020-04-01 20:16:09 +0530

    added a dma array prog

---
+++ w6/p2.cpp
@@ -0,0 +1,29 @@
+// fn to strcmp
+
+#include<iostream>
+#include<cstring>
+
+int strcomp(char*, char*);
+
+int main()
+{
+    char * st1 = new char[10];
+    char *st2 = new char[10];
+    strcpy(st1 , "bad");
+    strcpy(st2 , "bad");
+    
+    std::cout<<"the out put of comparing bad and Bad is: "<< strcomp(st1, st2);
+}
+
+int strcomp(char * st1, char* st2)
+{
+    int i=0,a = sizeof(st1)/sizeof(st1[0]), b = sizeof(st2)/sizeof(st2[0]);
+    while (i<=a && i<=b)
+    {
+        if (st1[i]!= st2[i]) return 1;
+        i++;
+    }
+    if (a!=b)return 1;
+    return 0;
+
+}
